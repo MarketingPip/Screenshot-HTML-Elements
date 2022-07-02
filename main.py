b6a632e5e04fe7e3565aa8f59957503e185851ee
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-07-01 22:11:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-import chromedriver_autoinstaller
+from webdriver_manager.chrome import ChromeDriverManager
 import os
 import json
 import sys
@@ -32,10 +32,8 @@ display.start()
 
 
 
-chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
-                                      # and if it doesn't exist, download it automatically,
-                                      # then add chromedriver to path
 
+driver = webdriver.Chrome(ChromeDriverManager().install())
 
 
 chrome_options = webdriver.ChromeOptions()
