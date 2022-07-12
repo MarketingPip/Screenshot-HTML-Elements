f1540602d90478171a65646e97cd108b166fe59c
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-07-12 02:14:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -116,7 +116,7 @@ for i in Files:
 
 
 def stream():
-    ffmpeg_stream = 'ffmpeg -video_size 640x480 -framerate 25 -f x11grab -i :0.0+100,200 output.mp4'
+    ffmpeg_stream = 'ffmpeg -f x11grab -s 1280x720 -r 24 -i :%d+nomouse -c:v libx264 -preset superfast -pix_fmt yuv420p -s 640x480 -threads 0 -f avi output.avi'
     args = shlex.split(ffmpeg_stream)
     p = subprocess.Popen(args)
 
